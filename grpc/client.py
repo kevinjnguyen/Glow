8e4c2905b2a74f95b86ca1a5f35902e305d0b9f8
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-02-15 23:58:35 -0600

    Added grpc protoc and client.py with working connection ri

--- grpc/client.py
+++ grpc/client.py
@@ -10,7 +10,7 @@ def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('192.168.43.144:50051') as channel:
         stub = grpc_pb2_grpc.GlowStub(channel)
         response = stub.TestPointReceiving(grpc_pb2.PointRequest(x=1,y=2))
     print("Greeter client received: " + response.message)
