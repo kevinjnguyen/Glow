7cc691f06704638014e32479e8e79a6007c1333b
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-02-16 15:06:39 -0600

    Added potential queue.

--- grpc/server.py
+++ grpc/server.py
@@ -10,6 +10,8 @@ import grpc
 import grpc_pb2
 import grpc_pb2_grpc
 
+from collections import deque
+
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 GPIO.setmode(GPIO.BOARD)
 
@@ -19,6 +21,10 @@ GPIO.setup(33, GPIO.OUT)
 
 bottom_servo = GPIO.PWM(12, 50)
 top_servo = GPIO.PWM(33, 50)
+
+mutex = Lock()
+
+queue = deque()
 
 def laser_on():
     GPIO.output(31, True)
@@ -40,29 +46,28 @@ def move_servos(pan_pwm, tilt_pwm):
     top_servo.ChangeDutyCycle(tilt_pwm)
     return ""
 
+def scale_ipad_to_box(request):
+    result = {}
+    result['x1'] = (request.x1/10.0 - 384.0) / 39.38
+    result['x2'] = (request.x2/10.0 - 384.0) / 39.38
+    result['y1'] = (request.y1/10.0 - 640.0) / -39.38
+    result['y2'] = (request.y2/10.0 - 640.0) / -39.38
+    return result
+
+def scale_box_to_pan_tilt(request):
+    pan_angle = math.atan(request['x2'] / 9.75)
+    tilt_angle = (math.pi / 2) - math.acos(request['y2'] / math.sqrt( request['x2'] ** 2 + request['y2'] **2 + 9.75**2))
+    return pan_angle, tilt_angle
+
+
 class Greeter(grpc_pb2_grpc.GlowServicer):
 
-    mutex = Lock()
-
-    def scale_ipad_to_box(self, request):
-        result = {}
-        result['x1'] = (request.x1/10.0 - 384.0) / 39.38
-        result['x2'] = (request.x2/10.0 - 384.0) / 39.38
-        result['y1'] = (request.y1/10.0 - 640.0) / -39.38
-        result['y2'] = (request.y2/10.0 - 640.0) / -39.38
-        return result
-
-    def scale_box_to_pan_tilt(self, request):
-        pan_angle = math.atan(request['x2'] / 9.75)
-        tilt_angle = (math.pi / 2) - math.acos(request['y2'] / math.sqrt( request['x2'] ** 2 + request['y2'] **2 + 9.75**2))
-        return pan_angle, tilt_angle
-
     def TestPointReceiving(self, request, context):
-        box_scaled = self.scale_ipad_to_box(request)
-        pan_angle, tilt_angle = self.scale_box_to_pan_tilt(box_scaled)
-        pan_pwm = angle_to_pwm_pan(pan_angle)
-        tilt_pwm = angle_to_pwm_tilt(tilt_angle)
-        move_servos(pan_pwm, tilt_pwm)
+        mutex.acquire()
+        try:
+            queue.append(request)
+        finally:
+            mutex.release()
         return grpc_pb2.GlowReply(message='Receieved!')
 
     def LotsOfPoints(self, request_iterator, context):
@@ -70,6 +75,21 @@ class Greeter(grpc_pb2_grpc.GlowServicer):
             print('(' + str(new_point.x1) + ', ' + str(new_point.y1) + ') ----> ' + '(' + str(new_point.x2) + ', ' + str(new_point.y2))
         return grpc_pb2.GlowReply(message='Receieved!')
 
+
+def processData():
+    mutex.acquire()
+    try:
+        print('PD: Acquired the lock.')
+        request = queue.popleft()
+        box_scaled = scale_ipad_to_box(request)
+        pan_angle, tilt_angle = scale_box_to_pan_tilt(box_scaled)
+        pan_pwm = angle_to_pwm_pan(pan_angle)
+        tilt_pwm = angle_to_pwm_tilt(tilt_angle)
+        move_servos(pan_pwm, tilt_pwm)
+    finally:
+        print('PD: Released the lock.')
+        mutex.release()
+    return 'Done'
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -79,14 +99,10 @@ def serve():
     laser_on()
     recenter()
 
-    bottom_servo.start(2)
-    time.sleep(0.5)
-    top_servo.start(2)
-    time.sleep(0.5)
-
     try:
         while True:
             time.sleep(_ONE_DAY_IN_SECONDS)
+        
     except KeyboardInterrupt:
         laser_off()
         top_servo.stop()
@@ -94,7 +110,15 @@ def serve():
         GPIO.cleanup()
         server.stop(0)
 
+def polling():
+    while True:
+        p = Thread(target = processData)
+        p.start()
+        time.sleep(0.2)
+        
 
 if __name__ == '__main__':
     logging.basicConfig()
     serve()
+    polling()
+
