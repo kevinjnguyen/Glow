02f183c636d44fb993dcb4ae6b1bf7719b6160ae
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-02-15 20:50:36 -0600

    Added initial Swift application.

---
+++ Glow/Glow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Glow
+//
+//  Created by Kevin J Nguyen on 2/15/19.
+//  Copyright © 2019 Kevin J Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
