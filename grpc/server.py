2af1b94f5b00b659c87ac2f27a71abaf464f76f2
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-02-16 15:29:04 -0600

    Added potential queue.

--- grpc/server.py
+++ grpc/server.py
@@ -22,10 +22,6 @@ GPIO.setup(33, GPIO.OUT)
 bottom_servo = GPIO.PWM(12, 50)
 top_servo = GPIO.PWM(33, 50)
 
-mutex = Lock()
-
-queue = deque()
-
 def laser_on():
     GPIO.output(31, True)
 
@@ -63,34 +59,17 @@ def scale_box_to_pan_tilt(request):
 class Greeter(grpc_pb2_grpc.GlowServicer):
 
     def TestPointReceiving(self, request, context):
-        mutex.acquire()
-        print('Server: Acquired the lock.')
-        try:
-            queue.append(request)
-        finally:
-            print('Server: Released the lock.')
-            mutex.release()
+        box_scaled = scale_ipad_to_box(request)
+        pan_angle, tilt_angle = scale_box_to_pan_tilt(box_scaled)
+        pan_pwm = angle_to_pwm_pan(pan_angle)
+        tilt_pwm = angle_to_pwm_tilt(tilt_angle)
+        move_servos(pan_pwm, tilt_pwm)
         return grpc_pb2.GlowReply(message='Receieved!')
 
     def LotsOfPoints(self, request_iterator, context):
         for new_point in request_iterator:
             print('(' + str(new_point.x1) + ', ' + str(new_point.y1) + ') ----> ' + '(' + str(new_point.x2) + ', ' + str(new_point.y2))
         return grpc_pb2.GlowReply(message='Receieved!')
-
-
-def processData():
-    mutex.acquire()
-    try:
-        if len(queue) > 0:
-            request = queue.popleft()
-            box_scaled = scale_ipad_to_box(request)
-            pan_angle, tilt_angle = scale_box_to_pan_tilt(box_scaled)
-            pan_pwm = angle_to_pwm_pan(pan_angle)
-            tilt_pwm = angle_to_pwm_tilt(tilt_angle)
-            move_servos(pan_pwm, tilt_pwm)
-    finally:
-        mutex.release()
-    return 'Done'
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -111,15 +90,8 @@ def serve():
         GPIO.cleanup()
         server.stop(0)
 
-def polling():
-    while True:
-        p = Thread(target = processData)
-        p.start()
-        time.sleep(1)
-        
 
 if __name__ == '__main__':
     logging.basicConfig()
-    # polling()
     serve()
 
